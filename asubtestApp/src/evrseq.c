50962eea557d37f6b20840917e8d20f97d736d26
Nicklas Holmberg <nicklas.holmberg@esss.se>
2018-09-19 15:52:05 +0000

    Faultfinding event and tick generation

--- asubtestApp/src/evrseq.c
+++ asubtestApp/src/evrseq.c
@@ -6,17 +6,45 @@
 #include <stdio.h>
 //#include <create_tick_event_list.h>
 
-int  create_tick_event_list(int freqs[], int arr_len, int sys_freq, int total_ticks, int base_event_no, int *tick_list, int *event_list) {
-        int i, j, k, total_events, ticks_per_event;
+int create_tick_event_list(int *freqs, int arr_len, int base_event_no, int *tick_list, int *event_list) {
+        int i, j, k, total_events, ticks_per_event, total_ticks, base_ticks, num_of_events, temp;
+	float sys_freq, float_freqs[arr_len];
+	temp = freqs[0];
+	sys_freq = (float) temp;
+	printf("create tick list input freq: %d arr_len: %d\n ", freqs[0], temp);
+	printf("input float temp:  %f, input float freq:  %f\n", sys_freq, float_freqs[0]);
+	for (i = 0; i < arr_len; i = i + 1) {
+		float_freqs[i] = (float) freqs[i];
+//		sys_freq = (float) freqs[0];
+	}
 	k = 0;
-        for(i = 0; i < arr_len; i = i + 1) {
-		ticks_per_event = total_ticks / freqs[i];
-		for(j = 0; j < freqs[i]; j = j + 1) {
-			event_list[k] = base_event_no+i;
-			tick_list[k] = j * ticks_per_event;
-			k = k + 1;
+	printf("Tick list input freqs: %d, %d, %d, %d\n", freqs[0], freqs[1], freqs[2], freqs[3]);
+	
+	base_ticks = 6289424;
+	total_ticks = base_ticks;
+	printf("input float temp:  %d, input float freq:  %d\n", sys_freq, float_freqs[0]);
+	sys_freq = 14.0;
+	for (i = 0; i < 4; i = i + 1) {
+		if ( sys_freq > freqs[i]) {
+			sys_freq = 14.0/12.0;
+			total_ticks = base_ticks * 12;
+			}
+	}
+        
+	for(i = 0; i < arr_len; i = i + 1) {
+		if (freqs[i] > 0) {
+			ticks_per_event = base_ticks / freqs[i];
+			num_of_events = (int) round(float_freqs[i]/sys_freq);
+			for(j = 0; j < num_of_events; j = j + 1) {
+				event_list[k] = base_event_no+i;
+				tick_list[k] = j * ticks_per_event;
+				k = k + 1;
+				printf("Event: %d Tick: %d\n", event_list[k], tick_list[k]);
+			}
 		}
         }
+
+	k = 999;
 	total_events = k;
         return total_events;
 
@@ -57,39 +85,38 @@ void sort_sequence(int arr_len, int *tick_list, int *event_list) {
 
 static int evr_sequence_modifier(aSubRecord *precord) {
 	
-	int in_freqs[4], in_delays_ns[4], std_tick_list[512], std_event_list[512];
-	int i, total_ticks, base_ticks, arr_len, base_event_no, total_events;
-	float low_freq;
+	float in_freqs[4], in_delays_ns[4], sys_freq, in_base_event_no;
+	int freqs[4], delays_ns[4], std_tick_list[512], std_event_list[512];
+	int i, total_ticks, base_ticks, arr_len, base_event_no, total_events, tmp;
 
 	printf("Start\n");
-	in_freqs[0] = *(int *)precord->a;
-	in_freqs[1] = *(int *)precord->b;
-	in_freqs[2] = *(int *)precord->c;
-	in_freqs[3] = *(int *)precord->d;
+	tmp = *(int *)precord->a; 
+	in_freqs[0] = *(float *)precord->a;
+	in_freqs[1] = *(float *)precord->b;
+	in_freqs[2] = *(float *)precord->c;
+	in_freqs[3] = *(float *)precord->d;	
+	in_delays_ns[0] = *(float *)precord->e;
+	in_delays_ns[1] = *(float *)precord->f;
+	in_delays_ns[2] = *(float *)precord->g;
+	in_delays_ns[3] = *(float *)precord->h;
 
-	in_delays_ns[0] = *(int *)precord->e;
-	in_delays_ns[1] = *(int *)precord->f;
-	in_delays_ns[2] = *(int *)precord->g;
-	in_delays_ns[3] = *(int *)precord->h;
 
-	base_event_no = *(int *)precord->i; //remove i from integers?
+	for (i = 0; i < 4; i = i +1) {
+		freqs[i] = (int)in_freqs[i];
+		delays_ns[i] = (int)in_delays_ns[i];
+	}	
 
-	base_ticks = 6289424;	//int *p_tick_event_list;
-	total_ticks = base_ticks;
-	low_freq = 14.0;
-	for (i = 0; i < 4; i = i + 1) {
-		if ( low_freq > in_freqs[i]) {
-			low_freq = 14.0/12.0;
-			total_ticks = base_ticks * 12;
-		}
-	}
-	printf("Input freqs: %d, %d\n", sizeof(in_freqs[0]), sizeof(in_freqs));
-	/*arr_len = sizeof(in_freqs) / sizeof(in_freqs[0]);
-	total_events = create_tick_event_list(in_freqs, arr_len, low_freq, total_ticks, base_event_no, std_tick_list, std_event_list);
+	in_base_event_no = *(float *)precord->i; //remove i from integers?
+	base_event_no = (int) in_base_event_no;
+	
+	arr_len = sizeof(freqs) / sizeof(freqs[0]);
 
+	total_events = create_tick_event_list(freqs, arr_len, base_event_no, std_tick_list, std_event_list);
+	printf("Total events: %d\n");
+/*
 	int tick_list[total_events], event_list[total_events];
 	arr_len = sizeof(tick_list) / sizeof(tick_list[0]);
-	apply_delay(arr_len, in_delays_ns, tick_list, event_list);
+	apply_delay(arr_len, delays_ns, tick_list, event_list);
 
 	arr_len = sizeof(tick_list) / sizeof(tick_list[0]);
 	sort_sequence(arr_len, tick_list, event_list);
--- iocBoot/iocasubtest/st.cmd
+++ iocBoot/iocasubtest/st.cmd
@@ -24,30 +24,11 @@ dbLoadRecords("db/modifyevrseq.db","SYS=$(SYS), EVR=$(EVR), DEV1=$(DEV1), DEV2=$
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
-
 # Select trigger source for soft seq 0, trigger source 0, 1 means pulser 1
-dbpf LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-TrigSrc-0-Sel 2
-
-# Load sequencer setup
-dbpf LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-Load-Cmd 1
-
-# Enable sequencer
-dbpf LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-Enable-Cmd 1
-
-# event code 14 (14 Hz)
-# 123 is the prescaler sync reset, 127 is the end of sequence, 
-# option -a is array
+#dbpf LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-TrigSrc-0-Sel 2
+#dbpf LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-Load-Cmd 1
+#dbpf LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-Enable-Cmd 1
 #caput -a LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-EvtCode-SP 2 17 127 #6289464 ticks for 14 Hz speed 
-
-# Defining time at which the event codes are sent
-# event 127 should be set 3 ticks before maximum sequence length
-# sequencer takes 12 ticks to start,should be compensated with 136ns?
 #3144712 6289424 12578848 25157696
 #caput -a LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-Timestamp-SP 2 0 12578845
-
 #caput -n LabS-Utgard-VIP:TS-EVR-1:SoftSeq0-Commit-Cmd 1
-
-
-
-## Start any sequence programs
-#seq sncxxx,"user=root"
